8e294f1937c4229643d8a81791ad68cc068b2468
Alejandro <alejandrof2003@gmail.com>
2026-06-26 18:32:11 +0200

    incorporacion MgTeMoO6

---
+++ Surrogates/CD/MgTeMoO6, MgTeMoO6/Train/train_r_total.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import sys
+
+ROOT_PATH = Path(__file__).resolve().parents[4]
+sys.path.insert(0, str(ROOT_PATH / "TMM"))
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+from training_r_total import train_models
+
+directory   = ROOT_PATH / "Models" / "R_total" / "MgTeMoO6_MgTeMoO6"
+database    = ROOT_PATH / "Datasets" / "CD" / "MgTeMoO6_MgTeMoO6"
+ntrain      = 8000
+nvalidation = 2000
+
+train_models(str(directory), str(database), ntrain, nvalidation)
